2dfdd736fddbf74c1b29a4393975f646b040ebab
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-07-26 07:37:41 +0100

    First draft of build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
+npm install
+ng build
+docker build -t andykuszyk/thobl:$commit .
+docker push andykuszyk/thobl:$commit
+sed -i "s/\[version\]/$commit/g" docker-compose.yml
