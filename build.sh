7c178f7b8773b7b2d8c4d47ef8fe772b4df7370a
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-07-28 19:49:19 +0100

    Added deployment to build

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ ng build
 docker build -t andykuszyk/thobl:$commit .
 docker push andykuszyk/thobl:$commit
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk" 
