7692c29f6d7bd9bdc7823e9c09cbd6f27e34fc1f
Andy Kuszyk <andykuszyk@live.co.uk>
2018-08-05 19:05:15 +0100

    Started adding google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SocialLoginModule, AuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
 import { AppComponent } from './app.component';
 import { SpaceComponent } from './space/space.component';
+
+let config = new AuthServiceConfig([
+    {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("893216655393-43s8tkkak2nj01r9anm958hharqib468.apps.googleusercontent.com")
+    }
+]);
+
+export function provideConfig() {
+    return config;
+}
 
 @NgModule({
   declarations: [
@@ -11,9 +23,16 @@ import { SpaceComponent } from './space/space.component';
   ],
   imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      SocialLoginModule
   ],
-  providers: [],
+    providers: [
+        {
+            provide: AuthServiceConfig,
+            useFactory: provideConfig
+        }
+
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
